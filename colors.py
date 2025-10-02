89e5ff8f23b4780f765c08c73d14662f6511f97d
Angelou de Bedout <angeloudebedout@gmail.com>
2025-10-01 17:59:29 -0700

    Add HISPANIC color map

---
+++ colors.py
@@ -0,0 +1,17 @@
+SEX = {"Male": "#1f77b4", "Female": "#d62728"}
+
+RACE = {
+    "White": "#8c564b",
+    "Black or African American": "#2ca02c",
+    "Asian": "#9467bd",
+    "American Indian and Alaska Native": "#bcbd22",
+    "Native Hawaiian and Other Pacific Islander": "#17becf",
+    "Some Other Race": "#7f7f7f",
+    "Two or More Races": "#e377c2",
+}
+
+AGE = {"<18": "#1f77b4", "18–34": "#ff7f0e", "35–64": "#2ca02c", "65+": "#9467bd"}
+HISPANIC = {
+    "Hispanic or Latino": "#ff7f0e",
+    "Not Hispanic or Latino": "#7f7f7f",
+}
